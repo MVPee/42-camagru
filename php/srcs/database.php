0db9f20a18274155b981ce98a6aa4055d2021df2
MVPee <mariusvanpee@hotmail.be>
2024-12-20 13:24:27 +0100

    Camagru start. Files implementations. Create all wanted containers with Docker.

---
+++ php/srcs/database.php
@@ -0,0 +1,13 @@
+<?php
+    try {
+        $conn = mysqli_connect(
+            $_ENV["DB_HOST"],
+            $_ENV["MYSQL_ROOT"],
+            $_ENV["MYSQL_ROOT_PASSWORD"],
+            $_ENV["MYSQL_DATABASE"]
+        );
+    }
+    catch (mysqli_sql_exception $e) {
+        echo "Failed to connect to database: " . $e->getMessage();
+    }
+?>
