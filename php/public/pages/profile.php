d21783ba5451e5883cb619c3de4dcb5515fed179
MVPee <mariusvanpee@hotmail.be>
2024-12-20 17:49:02 +0100

    Add data in profile page

--- php/public/pages/profile.php
+++ php/public/pages/profile.php
@@ -1,4 +1,8 @@
 <h1>Profile</h1>
 <?php
-    echo "<h2>Hello " . $_SESSION["username"] . "!</h2>";
+    $query = "SELECT * FROM users WHERE username = '" . $_SESSION["username"] . "'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+    echo "<p>Username: " . $row["username"] . "</p>";
+    echo "<p>Member since: " . $row["created_at"] . "</p>";
 ?>
