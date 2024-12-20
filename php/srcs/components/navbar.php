f7c86846750ce7b8ebcacb91aba0b53c6544af98
MVPee <mariusvanpee@hotmail.be>
2024-12-20 17:43:31 +0100

    Put components in private.

---
+++ php/srcs/components/navbar.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <?php if (isset($_SESSION["username"])) { ?>
+            <li><a href="/profile/">Profile</a></li>
+            <li><a href="/logout/">Logout</a></li>
+        <?php } else { ?>
+            <li><a href="/login/">Login</a></li>
+            <li><a href="/register/">Register</a></li>
+        <?php } ?>
+    </ul>
+</nav>
